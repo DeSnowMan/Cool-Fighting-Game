f33aa8c41bc0a0d6624b31e6fdf25f1812eff50e
DeSnowMan <112641348+DeSnowMan@users.noreply.github.com>
2022-12-12 09:59:04 -0700

    Add files via upload
    Finished the base game and now only have visuals and audio stuff to create

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let ctx = cnv.getContext('2d')
 
 document.addEventListener("keydown", movement)
 document.addEventListener("keyup", stopmove)
-
+// epik variables
 let GameTime = 0;
+let Timer = 240
+let GameState = "Menu"
 let Player1 = {
     x: 25,
     y: 50,
@@ -50,9 +52,67 @@ let Player2 = {
     },
 }
 
-setInterval(UpdateTime, 125)
-function UpdateTime () {
-    GameTime+= 1
+
+// updates game time at 1/1th of a second
+setInterval(UpdateTime, 1000)
+function UpdateTime() {
+    GameTime += 1
+    CountDown()
+}
+
+// I know you're watching
+function CountDown() {
+    if ( GameState === "Play") {
+        Timer -= 1
+    }
+}
+
+function reset() {
+    GameState = "Play"
+    Player1 = {
+        x: 25,
+        y: 50,
+        w: 60,
+        h: 140,
+        jump: false,
+        Crouching: false,
+        moveX: 0,
+        moveY: 9.81,
+        health: 10,
+        JumpCount: 0,
+        face: "right",
+        State: "free",
+        attackBox: {
+            w: 80,
+            h: 40,
+            x: 0,
+            y: 0,
+            c: 'Yellow'
+        },
+    }
+
+    Player2 = {
+        x: 960,
+        y: 50,
+        w: 60,
+        h: 140,
+        jump: false,
+        Crouching: false,
+        moveX: 0,
+        moveY: 9.81,
+        health: 10,
+        JumpCount: 0,
+        face: "left",
+        State: "free",
+        attackBox: {
+            w: 80,
+            h: 40,
+            x: 0,
+            y: 0,
+        },
+    }
+
+    Timer = 240
 }
 
 // simulates gravity of da earth which is -9.81 m/s
@@ -62,15 +122,15 @@ function Gravity() {
     } else if (Player1.y <= 320 && Player1.jump === false) {
         Player1.moveY = 9.81
     }
-    if (Player2.y >= 320 ) {
+    if (Player2.y >= 320) {
         Player2.y = 320
-    } else if (Player2.y <= 320 && Player2.jump === false ) {
+    } else if (Player2.y <= 320 && Player2.jump === false) {
         Player2.moveY = 9.81
     }
-    if (Player1.y <= 20 ) {
+    if (Player1.y <= 20) {
         Player1.moveY = 9.81
     }
-    if (Player2.y <= 20 ) {
+    if (Player2.y <= 20) {
         Player2.moveY = 9.81
     }
 }
@@ -80,14 +140,14 @@ function Jump(Player) {
     if (Player === 1 && Player1.JumpCount > 2) {
         Player1.moveY = 12
         console.log("YaFalled")
-    } else if ( Player === 2 && Player2.JumpCount > 2) {
+    } else if (Player === 2 && Player2.JumpCount > 2) {
         console.log("YaFalled")
         Player2.moveY = 12
     }
     if (Player === 1 && Player1.JumpCount <= 2) {
         Player1.moveY = -12
         console.log("Yajumped")
-    } else if ( Player === 2 && Player2.JumpCount <= 2) {
+    } else if (Player === 2 && Player2.JumpCount <= 2) {
         console.log("Yajumped")
         Player2.moveY = -12
     }
@@ -96,7 +156,7 @@ function Jump(Player) {
 // Draws health UI
 function DrawUI() {
     // the Random Letters here are just for calculations, because I couldn't be bothered to give em a unique name for each
-    let B = 8
+    let B = 36
 
     if (Player1.health < 5) {
         let C = Player1.health - 5
@@ -112,8 +172,8 @@ function DrawUI() {
         A += 15
     }
 
-    
-    let E = 8
+
+    let E = 36
 
     if (Player2.health < 5) {
         let T = Player2.health - 5
@@ -131,40 +191,40 @@ function DrawUI() {
 
 }
 
-MovePlayers = () =>  {
+MovePlayers = () => {
     Player1.y += Player1.moveY
     Player1.x += Player1.moveX
-    
+
     Player2.x += Player2.moveX
     Player2.y += Player2.moveY
 }
 
 function DrawPlayers() {
     let P1height = 140
-    
+
     ctx.fillStyle = 'lime'
     ctx.fillRect(Player1.x, Player1.y, Player1.w, Player1.h)
-    
+
     ctx.fillStyle = 'Red'
     ctx.fillRect(Player2.x, Player2.y, Player2.w, Player2.h)
 }
 
 DrawStuff = () => {
-    ctx.clearRect(0,0, 1264, 480)
+    ctx.clearRect(0, 0, 1264, 480)
 }
 
 // Handles Players controls with Event Listener
 function movement(_event) {
-    
+
     if (event.code == "ArrowUp" && Player2.JumpCount <= 2) {
         Player2.jump = true
         Jump(2)
     } else if (event.code == "ArrowDown") {
         Player2.Crouching = true
-    } else if (event.code == "ArrowLeft" && Player2.State === "free") {
+    } else if (event.code == "ArrowLeft") {
         Player2.moveX = -10
         Player2.face = "left"
-    } else if (event.code == "ArrowRight" && Player2.State === "free") {
+    } else if (event.code == "ArrowRight") {
         Player2.moveX = 10
         Player2.face = "right"
     }
@@ -180,52 +240,66 @@ function movement(_event) {
     } else if (event.code == "KeyD") {
         Player1.moveX = 10
         Player1.face = "right"
-    } else if ( event.code == "KeyJ" && Player1.State === "attack") {
+    } else if (event.code == "KeyJ" && Player1.State === "attack") {
         Attack(1)
     } else if (event.code == "KeyK" && Player1.State === "attack") {
         HeavyAttack(1)
-    } else if ( event.code == "KeyL" && Player1.State === "free") {
+    } else if (event.code == "KeyL" && Player1.State === "free") {
         Player1.State = "attack"
-    } else if (event.code == "KeyK" && Player1.State === "attack") {
-       Player1.State = "free" 
+    } else if (event.code == "KeyL" && Player1.State === "attack") {
+        Player1.State = "free"
     }
-    
-    
-    
+
+
+
     if (event.code == "Numpad1" && Player2.State === "attack") {
         Attack(2)
     } else if (event.code == "Numpad2" && Player2.State === "attack") {
         HeavyAttack(2)
     } else if (event.code == "Numpad3" && Player2.State === "attack") {
         Player2.State = "free"
-    } else if ( event.code == "Numpad3" && Player2.State === "free") {
+    } else if (event.code == "Numpad3" && Player2.State === "free") {
         Player2.State = "attack"
     } else if (event.code == "Numpad4") {
-       
-    }   else if (event.code == "Numpad5") {
+
+    } else if (event.code == "Numpad5") {
+    }
+
+    if( event.code === "Space" && GameState === "Menu") {
+        GameState = "Play"
+    } else if ( event.code === "Space" && GameState === "End") {
+        reset()
     }
 }
 
-function RenderAttacks() {
-    if (Player1.face === "right") {
-        Player1.attackBox.x = Player1.x + 40
-    } else {
-        Player1.attackBox.x = Player1.x - 60
-    }
-    
-    Player1.attackBox.y = Player1.y + 20
-    ctx.fillStyle = 'Yellow'
-    ctx.fillRect(Player1.attackBox.x, Player1.attackBox.y, Player1.attackBox.w, Player1.attackBox.h)
+function RenderAttacks( ) {
 
-    if (Player2.face === "right") {
-        Player2.attackBox.x = Player2.x + 40
-    } else {
-        Player2.attackBox.x = Player2.x - 60
+    if (Player1.State === "attack") {
+        if (Player1.face === "right") {
+            Player1.attackBox.x = Player1.x + 40
+        } else {
+            Player1.attackBox.x = Player1.x - 60
+        }
+
+        Player1.attackBox.y = Player1.y + 20
+        ctx.fillStyle = 'Yellow'
+        ctx.fillRect(Player1.attackBox.x, Player1.attackBox.y, Player1.attackBox.w, Player1.attackBox.h)
     }
-    
-    Player2.attackBox.y = Player2.y + 20
-    ctx.fillStyle = 'Cyan'
-    ctx.fillRect(Player2.attackBox.x, Player2.attackBox.y, Player2.attackBox.w, Player2.attackBox.h)
+
+    if (Player2.State === "attack") {
+        if (Player2.face === "right") {
+            Player2.attackBox.x = Player2.x + 40
+        } else {
+            Player2.attackBox.x = Player2.x - 60
+        }
+
+        Player2.attackBox.y = Player2.y + 20
+        ctx.fillStyle = 'Cyan'
+        ctx.fillRect(Player2.attackBox.x, Player2.attackBox.y, Player2.attackBox.w, Player2.attackBox.h)
+
+
+    }
+
 }
 
 function Wraparound() {
@@ -243,86 +317,85 @@ function Wraparound() {
 
 
 function Attack(Player) {
-    
+
     if (Player === 1 && Player1.State === "attack") {
         Player1.attackBox.h = 40
-        
-        if ( Player1.attackBox.x < Player2.x + Player2.w &&
+
+        if (Player1.attackBox.x < Player2.x + Player2.w &&
             Player1.attackBox.x + Player1.attackBox.w > Player2.x &&
             Player1.attackBox.y < Player2.y + Player2.h &&
             Player1.attackBox.h + Player1.attackBox.y > Player2.y && Player === 1
-           ) {
-           Player2.health-= 1
-           Player1.attackBox.c = "red"
-           console.log(`YOU'RE HURTING HIM`)
-       
-           if (Player1.face === "left") {
-               Player2.moveX += -24
-           } else {
-               Player2.moveX += 24
-           }
+        ) {
+            Player2.health -= 1
+            Player1.attackBox.c = "red"
+            console.log(`YOU'RE HURTING HIM`)
+
+            if (Player1.face === "left") {
+                Player2.moveX += -24
+            } else {
+                Player2.moveX += 24
+            }
         }
     }
 
-    if ( Player === 2 && Player2.State === "attack") {
+    if (Player === 2 && Player2.State === "attack") {
         Player2.attackBox.h = 40
-        if ( Player2.attackBox.x < Player1.x + Player1.w &&
+        if (Player2.attackBox.x < Player1.x + Player1.w &&
             Player2.attackBox.x + Player2.attackBox.w > Player1.x &&
             Player2.attackBox.y < Player1.y + Player1.h &&
             Player2.attackBox.h + Player2.attackBox.y > Player1.y
-           ) {
-           Player1.health-= 1
-           Player2.attackBox.c = "red"
-           console.log(`YOU'RE HURTING HIM`)
-        
-           if (Player2.face === "left") {
-               Player1.moveX += -12
-           } else {
-               Player1.moveX += 12
-           }
+        ) {
+            Player1.health -= 1
+            Player2.attackBox.c = "red"
+            console.log(`YOU'RE HURTING HIM`)
+
+            if (Player2.face === "left") {
+                Player1.moveX += -12
+            } else {
+                Player1.moveX += 12
+            }
         }
     }
 }
 
 function HeavyAttack(Player) {
-    
+
     if (Player === 1 && Player1.State === "attack") {
         Player1.attackBox.h = 80
-        
-        if ( Player1.attackBox.x < Player2.x + Player2.w &&
+
+        if (Player1.attackBox.x < Player2.x + Player2.w &&
             Player1.attackBox.x + Player1.attackBox.w > Player2.x &&
             Player1.attackBox.y < Player2.y + Player2.h &&
             Player1.attackBox.h + Player1.attackBox.y > Player2.y && Player === 1
-           ) {
-           Player2.health-= 1
-           Player1.attackBox.c = "red"
-           console.log(`YOU'RE HURTING HIM`)
-       
-           if (Player1.face === "left") {
-               Player2.moveX += -24
-           } else {
-               Player2.moveX += 24
-           }
+        ) {
+            Player2.health -= 1
+            Player1.attackBox.c = "red"
+            console.log(`YOU'RE HURTING HIM`)
+
+            if (Player1.face === "left") {
+                Player2.moveX += -24
+            } else {
+                Player2.moveX += 24
+            }
         }
     }
 
-    if ( Player === 2 && Player2.State === "attack") {
-        Player2.State = "Attack"
+    if (Player === 2 && Player2.State === "attack") {
         Player2.attackBox.h = 80
-        if ( Player2.attackBox.x < Player1.x + Player1.w &&
+        if (Player2.attackBox.x < Player1.x + Player1.w &&
             Player2.attackBox.x + Player2.attackBox.w > Player1.x &&
             Player2.attackBox.y < Player1.y + Player1.h &&
             Player2.attackBox.h + Player2.attackBox.y > Player1.y
-           ) {
-           Player1.health-= 1
-           Player2.attackBox.c = "red"
-           console.log(`YOU'RE HURTING HIM`)
-        
-           if (Player2.face === "left") {
-               Player1.moveX += -80
-           } else {
-               Player1.moveX += 80
-           }
+        ) {
+            Player1.health -= 1
+            Player2.attackBox.c = "red"
+            console.log(`YOU'RE HURTING HIM`)
+
+            if (Player2.face === "left") {
+                Player1.moveX += -80
+            } else {
+                Player1.moveX += 80
+            }
         }
     }
 }
@@ -353,10 +426,64 @@ function stopmove() {
     }
 }
 
-// animation stuff
-requestAnimationFrame(Game)
-function Game() {
-    console.log(GameTime)
+function drawTimer() {
+    ctx.fillStyle = "red"
+    ctx.fillRect (560, 0, 60 ,60)
+    ctx.fillStyle = "yellow"
+    ctx.font = "20px Georgia";
+    ctx.fillText (Timer, 570, 30)
+
+}
+
+
+function GameStatesHandler() {
+    if (GameState === "Play") {
+        DrawGame()
+    } else if (GameState === "Menu") {
+        DrawMenu()
+    } else if (GameState === "End") {
+        DrawDeath()
+    }
+}
+
+function DrawDeath() {
+    
+    ctx.font = "80px Georgia";
+
+    if (Player1.health > 0) { 
+        ctx.fillStyle = 'lime'
+        ctx.fillText (`Player 1 Wins!`, 10, 80)
+    } else {
+        ctx.fillStyle = 'red'
+        ctx.fillText (`Player 2 Wins!`, 10, 80)
+    }
+    ctx.fillStyle = 'white'
+    ctx.font = "20px Georgia";
+    ctx.fillText (`Press Space To restart`, 500, 420)
+}
+
+function DrawMenu() {
+
+}
+
+function CheckDeath() {
+    if (Timer <= 0) {
+        GameState = "End"
+    } else if (Player1.health <= 0 || Player2.health <= 0) {
+        GameState = "End"
+        console.log("Dead")
+    }
+}
+
+function DrawMenu() {
+    ctx.font = "80px Georgia";
+    ctx.fillStyle = 'white'
+    ctx.fillText (`epic Fighting Game`, 290, 80)
+    ctx.font = "20px Georgia";
+    ctx.fillText (`Press Space To start`, 500, 420)
+}
+
+function DrawGame() {
     DrawStuff()
     DrawUI()
     MovePlayers()
@@ -364,5 +491,13 @@ function Game() {
     Gravity()
     Wraparound()
     RenderAttacks()
+    drawTimer()
+    CheckDeath()
+}
+// animation stuff
+requestAnimationFrame(Game)
+function Game() {
+    console.log(GameTime)
+    GameStatesHandler()
     requestAnimationFrame(Game)
 }
